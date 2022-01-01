5d4ba289d96cef5e485856372a4a4253474ddeec
Nnad Chime <nnadi1chime@gmail.com>
2022-01-01 02:40:00 +0100

    notify app

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let input = document.getElementById("input");
+let sweetAlert = document.querySelector(".sweetAlert");
+
+input.addEventListener("click", () => {
+sweetAlert.classList.add("show");
+})
